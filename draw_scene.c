8953022849a63398a838e69a8820df260f77a98b
vancolbert <177286069+vancolbert@users.noreply.github.com>
2024-10-15 01:52:01 -0400

    use tabs for indentation, fix line endings

--- draw_scene.c
+++ draw_scene.c
@@ -638,50 +638,50 @@ static int n_mapcams;
 #define countof(a) (sizeof(a)/sizeof(*a))
 #define for_mapcams(v) for (Mapcam *v = mapcams, *v##_e = v + n_mapcams; v < v##_e; ++v)
 static inline int distsq(v2i a, v2i b) {
-    int dx = a.x - b.x, dy = a.y - b.y;
-    return dx*dx + dy*dy;
+	int dx = a.x - b.x, dy = a.y - b.y;
+	return dx*dx + dy*dy;
 }
 static inline v2i get_our_pos(void) {
-    actor *a = get_our_actor();
-    return a ? (v2i){a->x_tile_pos, a->y_tile_pos} : (v2i){0, 0};
+	actor *a = get_our_actor();
+	return a ? (v2i){a->x_tile_pos, a->y_tile_pos} : (v2i){0, 0};
 }
 static inline Mapcam *find_mapcam(int map, v2i p) {
-    Mapcam *m = 0;
-    for_mapcams(i) {
-        if (i->map == map) {
-            int d = distsq(i->p, p);
-            if (d < 50) {
-                m = i;
-                break;
-            }
-        }
-    }
-    return m;
+	Mapcam *m = 0;
+	for_mapcams(i) {
+		if (i->map == map) {
+			int d = distsq(i->p, p);
+			if (d < 50) {
+				m = i;
+				break;
+			}
+		}
+	}
+	return m;
 }
 void stash_mapcam(void) {
-    Mapcam *m, c = {get_our_pos(), cur_map, cur_time, {rx, ry, rz, zoom_level}};
-    if (!(m = find_mapcam(c.map, c.p))) {
-        if (n_mapcams < countof(mapcams)) {
-            m = mapcams + n_mapcams++;
-        } else {
-            Uint32 oldest = cur_time;
-            for_mapcams(i) {
-                if (i->t < oldest) {
-                    oldest = i->t;
-                    m = i;
-                }
-            }
-        }
-    }
-    *m = c;
+	Mapcam *m, c = {get_our_pos(), cur_map, cur_time, {rx, ry, rz, zoom_level}};
+	if (!(m = find_mapcam(c.map, c.p))) {
+		if (n_mapcams < countof(mapcams)) {
+			m = mapcams + n_mapcams++;
+		} else {
+			Uint32 oldest = cur_time;
+			for_mapcams(i) {
+				if (i->t < oldest) {
+					oldest = i->t;
+					m = i;
+				}
+			}
+		}
+	}
+	*m = c;
 }
 void restore_mapcam(void) {
-    Mapcam *m = find_mapcam(cur_map, get_our_pos());
-    if (m) {
-        rx = m->r[0];
-        ry = m->r[1];
-        rz = m->r[2];
-        zoom_level = m->r[3];
-        m->t = cur_time;
-    }
+	Mapcam *m = find_mapcam(cur_map, get_our_pos());
+	if (m) {
+		rx = m->r[0];
+		ry = m->r[1];
+		rz = m->r[2];
+		zoom_level = m->r[3];
+		m->t = cur_time;
+	}
 }
