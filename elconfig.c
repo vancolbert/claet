9708954d4103e67989bc6a29e2be61463727a5b2
vancolbert <177286069+vancolbert@users.noreply.github.com>
2024-12-15 00:06:25 -0500

    remove "point" typedef usage

--- elconfig.c
+++ elconfig.c
@@ -2087,7 +2087,7 @@ void add_var(option_type type, char * name, char * shortname, void * var, void *
 	int no=our_vars.no++;
 	char *pointer;
 	float *tmp_f;
-	point *tmp_i;
+	int *tmp_i;
 	int_min_max_func *i_func;
 	float_min_max_func *f_func;
 	va_list ap;
@@ -2111,11 +2111,11 @@ void add_var(option_type type, char * name, char * shortname, void * var, void *
 			va_start(ap, tab_id);
 			//Min
 			tmp_i= calloc(1,sizeof(*tmp_i));
-			*tmp_i= va_arg(ap, point);
+			*tmp_i= va_arg(ap, int);
 			queue_push(our_vars.var[no]->queue, tmp_i);
 			//Max
 			tmp_i= calloc(1,sizeof(*tmp_i));
-			*tmp_i= va_arg(ap, point);
+			*tmp_i= va_arg(ap, int);
 			queue_push(our_vars.var[no]->queue, tmp_i);
 			va_end(ap);
 			*integer= (int)def;
