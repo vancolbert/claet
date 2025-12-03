17ea1cee004b74abca3cbbbe179b595872a31e25
vancolbert <177286069+vancolbert@users.noreply.github.com>
2025-12-02 22:33:49 -0500

    use new level table for SOFT_CAP

--- hud.h
+++ hud.h
@@ -32,6 +32,7 @@ extern int show_coord_2;
 #endif //SHOW_COORD_SETTER
 extern int rot_boussole; //Type de rotation de la boussole
 extern Uint32 exp_lev[200];
+extern int num_exp_lev;
 #else //ENGLISH
 /*!
  * \name Quickbar defines
